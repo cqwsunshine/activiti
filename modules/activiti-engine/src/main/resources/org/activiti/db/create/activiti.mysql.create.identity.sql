52691d540b931f2fe35bfa9a9fd0af6737ebbf56
qiwei <cuiqiwei_it@163.com>
2019-09-26 22:17:14 +0800

    modify 修改activiti-engin工程中act_id_user,act_id_group,act_id_membership添加系统标识systemId字段；修改activiti-app中pom文件，数据库驱动

---
+++ modules/activiti-engine/src/main/resources/org/activiti/db/create/activiti.mysql.create.identity.sql
@@ -0,0 +1,49 @@
+create table ACT_ID_GROUP (
+    ID_ varchar(64), -- 角色ID
+    REV_ integer,
+    NAME_ varchar(255),
+    TYPE_ varchar(255),
+    SYSTEMID_ varchar(255), -- 系统标识
+    primary key (ID_)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
+
+create table ACT_ID_MEMBERSHIP (
+    USER_ID_ varchar(64),-- 用户ID
+    GROUP_ID_ varchar(64), -- 角色ID
+    -- SYSTEMID_ varchar(255), -- 系统标识
+    primary key (USER_ID_, GROUP_ID_)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
+
+create table ACT_ID_USER (
+    ID_ varchar(64), -- 用户ID
+    REV_ integer,
+    FIRST_ varchar(255), -- 用户名
+    LAST_ varchar(255),
+    EMAIL_ varchar(255),
+    PWD_ varchar(255),
+    PICTURE_ID_ varchar(64),
+    -- SYSTEMID_ varchar(255), -- 系统标识
+    primary key (ID_)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
+
+create table ACT_ID_INFO (
+    ID_ varchar(64),
+    REV_ integer,
+    USER_ID_ varchar(64),
+    TYPE_ varchar(64),
+    KEY_ varchar(255),
+    VALUE_ varchar(255),
+    PASSWORD_ LONGBLOB,
+    PARENT_ID_ varchar(255),
+    primary key (ID_)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
+
+alter table ACT_ID_MEMBERSHIP 
+    add constraint ACT_FK_MEMB_GROUP 
+    foreign key (GROUP_ID_) -- 角色ID
+    references ACT_ID_GROUP (ID_);
+
+alter table ACT_ID_MEMBERSHIP 
+    add constraint ACT_FK_MEMB_USER 
+    foreign key (USER_ID_) -- 用户ID
+    references ACT_ID_USER (ID_);
